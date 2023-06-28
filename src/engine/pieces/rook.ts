cd1836155d99a2b564e4450c670f0a579bfac32d
Robert Grancsa <Robert.Grancsa@softwire.com>
2023-06-28 14:56:27 +0300

    Added rook moves
    Signed-off-by: Robert Grancsa <Robert.Grancsa@softwire.com>

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
+import player from "../player";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,55 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves: Square[] = []
+
+        const currentSquare = board.findPiece(this);
+        for (let i = currentSquare.col + 1; i < 8; i++) {
+            let nextSquare = Square.at(currentSquare.row, i);
+            let pieceOnSquare = board.getPiece(nextSquare);
+            if (pieceOnSquare !== undefined) {
+                if (pieceOnSquare.player != this.player) {
+                    moves.push(nextSquare);
+                }
+                break;
+            }
+            moves.push(nextSquare);
+        }
+        for (let i = currentSquare.col - 1; i >= 0; i--) {
+            let nextSquare = Square.at(currentSquare.row, i);
+            let pieceOnSquare = board.getPiece(nextSquare);
+            if (pieceOnSquare !== undefined) {
+                if (pieceOnSquare.player != this.player) {
+                    moves.push(nextSquare);
+                }
+                break;
+            }
+            moves.push(nextSquare);
+        }
+
+        for (let i = currentSquare.row + 1; i < 8; i++) {
+            let nextSquare = Square.at(i, currentSquare.col);
+            let pieceOnSquare = board.getPiece(nextSquare);
+            if (pieceOnSquare !== undefined) {
+                if (pieceOnSquare.player != this.player) {
+                    moves.push(nextSquare);
+                }
+                break;
+            }
+            moves.push(nextSquare);
+        }
+        for (let i = currentSquare.row - 1; i >= 0; i--) {
+            let nextSquare = Square.at(i, currentSquare.col);
+            let pieceOnSquare = board.getPiece(nextSquare);
+            if (pieceOnSquare !== undefined) {
+                if (pieceOnSquare.player != this.player) {
+                    moves.push(nextSquare);
+                }
+                break;
+            }
+            moves.push(nextSquare);
+        }
+
+        return moves;
     }
 }
