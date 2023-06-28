df70e2b0332c4da62926eea398a3db678573b33a
Robert Grancsa <Robert.Grancsa@softwire.com>
2023-06-28 16:00:46 +0300

    Fixed knight taking things
    Signed-off-by: Robert Grancsa <Robert.Grancsa@softwire.com>

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -2,6 +2,7 @@ import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
 import Square from "../square";
+import King from "./king";
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -22,6 +23,19 @@ export default class Knight extends Piece {
         moves.push(Square.at(currentSquare.row - 1, currentSquare.col - 2));
         moves.push(Square.at(currentSquare.row - 1, currentSquare.col + 2));
 
-        return moves.filter((e: Square) => e.row >= 0 && e.row < 8 && e.col >= 0 && e.col < 8);
+        moves = moves.filter((move: Square) => {
+            if (!(move.row >= 0 && move.row < 8 && move.col >= 0 && move.col < 8)) {
+                return false;
+            }
+
+            let piece = board.getPiece(move);
+            if (piece === undefined) {
+                return true;
+            }
+
+            return !!piece && piece.player != this.player && !(piece instanceof King);
+        })
+
+        return moves;
     }
 }
