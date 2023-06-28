de081fbd3b0aa99754426e4f6241bcc02b681e57
Robert Grancsa <Robert.Grancsa@softwire.com>
2023-06-28 15:13:25 +0300

    Added king moves
    Signed-off-by: Robert Grancsa <Robert.Grancsa@softwire.com>

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,18 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves: Square[] = []
+
+        const currentSquare = board.findPiece(this);
+        moves.push(Square.at(currentSquare.row + 1, currentSquare.col + 1));
+        moves.push(Square.at(currentSquare.row - 1, currentSquare.col + 1));
+        moves.push(Square.at(currentSquare.row + 1, currentSquare.col - 1));
+        moves.push(Square.at(currentSquare.row - 1, currentSquare.col - 1));
+        moves.push(Square.at(currentSquare.row, currentSquare.col + 1));
+        moves.push(Square.at(currentSquare.row - 1, currentSquare.col));
+        moves.push(Square.at(currentSquare.row + 1, currentSquare.col));
+        moves.push(Square.at(currentSquare.row, currentSquare.col - 1));
+
+        return moves;
     }
 }
